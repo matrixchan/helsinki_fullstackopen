75c718c09247c66963b617d8f7a3a02ed0609ef6
Matrixtech Ubuntu18.04 <immatrixchan@gmail.com>
2020-10-12 13:21:49 +0800

    1.1 完成,清理不需要的文件.

---
+++ part1/courseinfo-bak/src/index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+const Header = (props) => {
+  return (
+  <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props)=>{
+  console.log(props)
+  return (
+  <p> {props.part.name} {props.part.exercises} </p>
+  )
+
+}
+
+const Content = (props) => {
+   //console.log(props)
+  return (
+    <div>
+      <Part part = {props.parts[0]}  />
+      <Part part = {props.parts[1]} />
+      <Part part = {props.parts[2]}  />
+    </div>
+  )
+}
+
+const Total = (props) =>{
+  //console.log(props);
+  let exercises0 = props.parts[0].exercises;
+  let exercises1 = props.parts[1].exercises;
+  let exercises2 = props.parts[2].exercises;
+  let total = exercises0 + exercises1 +exercises2;
+
+  return(
+      <p>
+        Number of exercises {total}
+      </p>
+
+  )
+}
+
+
+const App = () => {
+  const course  = {
+    name: 'Half Stack application development',
+    parts: [{ name: 'Fundamentals of React',
+              exercises: 10
+              },
+            { name: 'Using props to pass data',
+              exercises: 7
+            },
+            { name: 'State of a component',
+              exercises: 14}]
+          }
+
+    
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts}  />
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,68 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-
-const Header = (props) => {
-  return (
-  <h1>{props.course}</h1>
-  )
-}
-
-const Part = (props)=>{
-  console.log(props)
-  return (
-  <p> {props.part.name} {props.part.exercises} </p>
-  )
-
-}
-
-const Content = (props) => {
-   //console.log(props)
-  return (
-    <div>
-      <Part part = {props.parts[0]}  />
-      <Part part = {props.parts[1]} />
-      <Part part = {props.parts[2]}  />
-    </div>
-  )
-}
-
-const Total = (props) =>{
-  //console.log(props);
-  let exercises0 = props.parts[0].exercises;
-  let exercises1 = props.parts[1].exercises;
-  let exercises2 = props.parts[2].exercises;
-  let total = exercises0 + exercises1 +exercises2;
+const App = ()=>{
+  const course = "Half Stack application development"
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return(
-      <p>
-        Number of exercises {total}
-      </p>
-
-  )
-}
-
-
-const App = () => {
-  const course  = {
-    name: 'Half Stack application development',
-    parts: [{ name: 'Fundamentals of React',
-              exercises: 10
-              },
-            { name: 'Using props to pass data',
-              exercises: 7
-            },
-            { name: 'State of a component',
-              exercises: 14}]
-          }
-
-    
-  return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts}  />
-      <Total parts={course.parts}/>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
