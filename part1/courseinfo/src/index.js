90429796eb08b1601123e66099d410cbce14cf12
Matrixtech Ubuntu18.04 <immatrixchan@gmail.com>
2020-10-12 18:56:07 +0800

    1.4 1.5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -19,9 +19,10 @@ const Content = (props)=>{
 
   return(
     <div>
-    <Part part={props.part1} exercises={props.exercises1} />
-    <Part part={props.part2} exercises={props.exercises2} />
-    <Part part={props.part3} exercises={props.exercises3} />
+    <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+    <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+    <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+
     </div>
 
   )
@@ -29,38 +30,36 @@ const Content = (props)=>{
 
 const Total = (props)=>{
   return(
-    <p>Number of exercises {props.total}</p>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} </p>
   )
 }
 
 const App = ()=>{
   const course = "Half Stack application development"
-  const part1 = {
-    name : 'Fundamentals of React',
-    exercises:10
-  }
+  const parts = [
+    {
+      name : 'Fundamentals of React',
+      exercises:10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
   
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
   
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
 
 
   return(
     <div>
-      <Header course={course}/>
-      <Content 
-        part1={part1} exercises1={exercises1}
-        part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3}
-      />
-      <Total total =  {exercises1 + exercises2 + exercises3}/>
-      
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
